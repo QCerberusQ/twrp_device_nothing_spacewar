f277347de4fd3bc007797781d340ed381122acbc
Furkan Enes <113352008+QCerberusQ@users.noreply.github.com>
2025-12-26 02:22:36 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -144,6 +144,7 @@ TW_EXTRA_LANGUAGES := true
 TW_INCLUDE_NTFS_3G := true
 TW_LOAD_VENDOR_MODULES := true
 TW_USE_TOOLBOX := true
+TW_USE_EVDEV_INPUT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
 TW_MAX_BRIGHTNESS := 2047
