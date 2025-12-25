333121d278081e8fac37cd1d4e5be924554b061e
Furkan Enes <113352008+QCerberusQ@users.noreply.github.com>
2025-12-26 02:27:12 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -84,7 +84,6 @@ TW_INCLUDE_NTFS_3G := true
 TW_USE_TOOLBOX := true
 TW_INCLUDE_RESETPROP := true
 TW_INCLUDE_REPACKTOOLS := true
-TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
